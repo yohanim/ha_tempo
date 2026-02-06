a10d9c24085b90450b3d57ee007b57df43a9ced4
yohanim <kevin.gossent@gmail.com>
2026-02-07 00:56:05 +0100

    fix: rte sensor available

--- custom_components/tempo_rte_forecast/tempo_sensor.py
+++ custom_components/tempo_rte_forecast/tempo_sensor.py
@@ -51,7 +51,7 @@ class TempoSensor(CoordinatorEntity, SensorEntity):
         """Le sensor est disponible si on a au moins des données en cache."""
         day = get_tempo_date(self.index)
         day_data = self.coordinator.get_data(day)
-        return get_color_code(day_data) != 0
+        return day_data != None
 
     @property
     def native_value(self) -> str:
