3ce5f8dd4e5cc7e87cee975c41dbc55f75081355
yohanim <kevin.gossent@gmail.com>
2026-02-07 14:00:01 +0100

    fix: config

--- custom_components/tempo_rte_forecast/config_flow.py
+++ custom_components/tempo_rte_forecast/config_flow.py
@@ -59,13 +59,9 @@ class TempoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Create the options flow."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 class OptionsFlowHandler(OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Manage the options."""
         if user_input is not None:
